ac387b6eb9b104bca1327d2cde0b1223953f3b88
jc <jchorzempa@posteo.org>
2023-01-30 23:43:14 +0100

    Add Robot class code

--- projects/from-javascript-to-typescript/the-typeinator/01-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-prototypes-to-classes/index.js
@@ -3,3 +3,21 @@
 
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
+
+
+class Robot {
+    constructor (name, abilities) {
+        this.name = name;
+        this.abilities = abilities;
+        this.power = 100;
+    }
+
+    announce () {
+        console.log("Greetings. I am " + this.name + ".");
+
+	for (var i = 0; i < this.abilities.length; i += 1) {
+		console.log("I am able to " + this.abilities[i] + ".");
+	}
+
+    }
+}
