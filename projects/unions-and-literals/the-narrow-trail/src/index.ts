dfaafcab185bd4498add7f563cf6f9a511eea14e
Johannes Chorzempa <110151013+jmchor@users.noreply.github.com>
2023-02-05 23:26:20 +0100

    Start ex 2 ch 2

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,8 @@
 export function runCommands() {
+	let food = 5;
+	let water = 5;
+	let availableRessource: "food" | "water" | undefined;
+
+	let diceRoll: number = Math.floor(Math.random() * 6) + 1;
 	// Declare your variables and runtime logic here! ✨
 }
