abc5b4e51a157f15c7b50d5b21929aa006d367f9
Johannes Chorzempa <110151013+jmchor@users.noreply.github.com>
2023-02-24 23:03:53 +0100

    Add comments

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,8 +1,10 @@
 // Write your createCipher function here! ✨
 
 export type Cipher = (character: string) => string;
+//create a type alias for a function that takes in a string and returns a string
 
 export function createCipher(cipher: Cipher) {
+	//create a function which takes in the previously typed function and has a return type of a EVERYTHING which is written after the first "return" (that is the Return Type of that function and very specific)
 	return (text: string) => {
 		let result = "";
 
