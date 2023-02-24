e5d8c5ae54cd580df5eeb0f7f036a357ceb820f1
Johannes Chorzempa <110151013+jmchor@users.noreply.github.com>
2023-02-24 23:01:04 +0100

    Solve ex 05-01-01 cipher

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,16 @@
 // Write your createCipher function here! ✨
+
+export type Cipher = (character: string) => string;
+
+export function createCipher(cipher: Cipher) {
+	return (text: string) => {
+		let result = "";
+
+		for (const character of text) {
+			result += cipher(character);
+		}
+		return result;
+	};
+}
+
 // You'll need to export it so the tests can run it.
