42d91a0d20bf433784addfb5be2ce48233cdec8f
Johannes Chorzempa <110151013+jmchor@users.noreply.github.com>
2023-02-25 22:49:29 +0100

    Solved 05-01-02

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,29 @@
 // Write your createAdvancedCipher function here! ✨
+
+export type Cipher = (character: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: Cipher,
+	onConsonant: Cipher,
+	onPunctuation: Cipher
+) {
+	let vowelReg = /[aeiou]/i;
+	let consReg = /[bcdfghjklmnpqrstvwxyz]/i;
+
+	return (text: string) => {
+		let result = "";
+
+		for (const character of text) {
+			if (character.match(vowelReg)) {
+				result += onVowel(character);
+			} else if (character.match(consReg)) {
+				result += onConsonant(character);
+			} else {
+				result += onPunctuation(character);
+			}
+		}
+		return result;
+	};
+}
+
 // You'll need to export it so the tests can run it.
