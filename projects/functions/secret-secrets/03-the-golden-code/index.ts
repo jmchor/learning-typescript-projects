630bd61e0cba9b80ac45b40c6188df6348bb3596
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-02 10:54:43 +0100

    Solve ex05-01-03

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,23 @@
 // Write your createCodeCracker function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type codeObject = {
+	attempts: number;
+	makeGuess: (text: string, attempt: number) => string;
+	validateGuess: (guess: string) => boolean;
+};
+
+export function createCodeCracker({
+	attempts,
+	makeGuess,
+	validateGuess,
+}: codeObject) {
+	return (text: string) => {
+		for (let i = 0; i < attempts; i++) {
+			const guess = makeGuess(text, i);
+			if (validateGuess(guess)) {
+				return guess;
+			}
+		}
+	};
+}
