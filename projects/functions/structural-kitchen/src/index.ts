de26db3b28d45486298839f9b3f740e2cfef6a4d
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-02 21:40:36 +0100

    Edit exercise 05-02

--- projects/functions/structural-kitchen/src/index.ts
+++ projects/functions/structural-kitchen/src/index.ts
@@ -2,4 +2,26 @@
 // You'll need to export it so the tests can run it.
 export type Cleaner = (dirt: number, time?: number) => number;
 
-export type Supplier = (expense: number) => {};
+export type Ingredients = {
+	breads: number;
+	fruits: number;
+	sauces: number;
+	vegetables: number;
+};
+
+export type Supplier = (expense: number) => Ingredients;
+
+export function createKitchen(
+	budget: number,
+	cleaner: Cleaner,
+	supplier: Supplier
+): {} {
+	let dirt = 0;
+	let stock = {
+		breads: 0,
+		fruits: 0,
+		sauces: 0,
+		vegetables: 0,
+	};
+	return {};
+}
