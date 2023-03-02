22f4c7e48d00b32ee01a73772ce8a58cd1c1744a
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-02 11:13:37 +0100

    Start ex05-02

--- projects/functions/structural-kitchen/src/index.ts
+++ projects/functions/structural-kitchen/src/index.ts
@@ -1,2 +1,5 @@
 // Write your createKitchen function here! ✨
 // You'll need to export it so the tests can run it.
+export type Cleaner = (dirt: number, time?: number) => number;
+
+export type Supplier = (expense: number) =>
